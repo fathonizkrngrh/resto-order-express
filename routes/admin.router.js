823a46cd9b03d32f8fa601a66620b347b40b1e2d
Fathoni Zikri Nugroho <fathonizikri11@gmail.com>
2023-01-02 22:49:44 +0700

    feature add category

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var router = express.Router();
 const productController = require("../controllers/admin/product.controller");
+const categoryController = require("../controllers/admin/category.controller");
 
-/* GET users listing. */
+/* Product */
 router.get("/product", productController.viewProduct);
+
+// Category
+router.get("/category", categoryController.viewCategory);
+router.post("/category", categoryController.addCategory);
 
 module.exports = router;
